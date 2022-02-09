f2c81d9596e9440d865a962164ea47224b2026da
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-09 19:25:48 -0400

    feat(Todo): add TodoSearch Component

---
+++ src/TodoSearch/index.js
@@ -0,0 +1,19 @@
+import './TodoSearch.css';
+
+function TodoSearch() {
+  const onSearchValueChanged = (event) => {
+    console.log(event.target.value);
+  }
+
+  return (
+    <input 
+      className="TodoSearch" 
+      placeholder="Cebolla" 
+      onChange={onSearchValueChanged}
+    />
+  );
+}
+
+export {
+  TodoSearch
+};
