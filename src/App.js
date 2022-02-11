fae987eb5d3c1713fc1d013800206fc519fbecf8
Gustavo Martínez <gustavomartinez1990@gmail.com>
2022-02-10 22:23:23 -0400

    feat(TodoSearch): filter todos using todo search component state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { TodoItem } from './TodoItem/';
 import { CreateTodoButton } from './CreateTodoButton/';
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el cursso de intro a React', completed: false },
   { text: 'Llorar con la llorona', completed: false },
@@ -14,18 +14,39 @@ const todos = [
 ];
 
 function App() {
+  const [todos, setTodos ] = useState(defaultTodos);
   const [searchValue, setSearchValue] = useState('');
+
+  const completedTodos = todos.filter((item) => !!item.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >=1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+
+      return todoText.includes(searchText);
+    });
+  }
+
 
   return (
     <React.Fragment>
-      <TodoCounter />
+      <TodoCounter 
+        total={totalTodos}
+        completed={completedTodos}
+      />
       <TodoSearch 
         searchValue={searchValue} 
         setSearchValue={setSearchValue}
       />
 
       <TodoList>
-        {todos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
